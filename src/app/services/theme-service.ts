7293f82b1c6a764d60e4c0174fa2517ca2165a0d
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-10-20 17:36:31 -0300

    feat
    - Começo do html e scss do header;
    - Criação do service para mudar o tema;

---
+++ src/app/services/theme-service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  private body = document.documentElement;
+
+  // verifica o tema atual, se nao tiver nenhum, aplica o dark como padrao
+  constructor () {
+    const actualTheme = localStorage.getItem("theme") || "dark-theme";
+    this.applyTheme(actualTheme);
+  }
+
+  // aplica a classe do tema no html
+  applyTheme(newTheme:string) {
+    this.body.classList.remove("dark-theme", "light-theme");
+    this.body.classList.toggle(newTheme);
+  }
+
+  // funcao publica para mudar o tema no cache e na classe html
+  changeTheme(theme: 'dark-theme' | 'light-theme') {
+    localStorage.setItem("theme", theme);
+    this.applyTheme(theme);
+  }
+}
