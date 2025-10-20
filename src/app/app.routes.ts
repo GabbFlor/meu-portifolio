f09b1f4672c93824a8e618a313df4d70b547b5a3
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-10-20 15:16:50 -0300

    feat
    - Estrutura padrão dos arquivos configurada;
    - Pré configurando a rota de erro;

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { HomePage } from './routes/home-page/home-page';
+import { EmptyLayout } from './layouts/empty-layout/empty-layout';
+import { ErrorNotFound } from './routes/error-not-found/error-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayout,
+        children: [
+            {
+                path: '',
+                component: HomePage
+            }
+        ]
+    },
+    {
+        path: '',
+        component: EmptyLayout,
+        children: [
+            {
+                path: 'not-found',
+                component: ErrorNotFound
+            }
+        ]
+    },
+    {
+        path: "**",
+        redirectTo: 'not-found'
+    }
+];
---
+++ src/app/layouts/empty-layout/empty-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from "../../components/header/header";
+
+@Component({
+  selector: 'app-empty-layout',
+  imports: [Header],
+  templateUrl: './empty-layout.html',
+  styleUrl: './empty-layout.scss'
+})
+export class EmptyLayout {
+
+}
