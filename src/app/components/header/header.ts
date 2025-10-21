0bffe30e94db50f7e56eebf44abd5e4a326c4fb7
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-10-21 12:59:29 -0300

    feat
    commit de backup (vou tentar melhorar o service de mudar tema)
    
    - Terminei a função de tema agr para ícones tb

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ThemeService } from '../../services/theme-service';
 
 @Component({
@@ -7,18 +7,26 @@ import { ThemeService } from '../../services/theme-service';
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header {
+export class Header implements OnInit {
+  public actualTheme:string|null = "";
+
   constructor (private themeService: ThemeService) {}
 
-  changeColorTheme() {
-    let actualTheme = this.themeService.getActualTheme();
+  ngOnInit(): void {
+    this.actualTheme = this.themeService.getActualTheme();
+  }
 
-    switch(actualTheme) {
+  changeColorTheme() {
+    const theme = this.actualTheme;
+
+    switch(theme) {
       case "dark-theme":
         this.themeService.changeTheme("light-theme");
+        this.actualTheme ="light-theme";
         break;
       case "light-theme":
         this.themeService.changeTheme("dark-theme");
+        this.actualTheme = "dark-theme";
         break;
       default:
         console.error("Erro no tema.")
