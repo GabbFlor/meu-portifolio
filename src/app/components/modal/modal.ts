80a84143cb9f77cf028ee209a0da6aaf7ff2e5b7
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-11-03 16:11:14 -0300

    test
    tentando fazer a porra do modal funcionar com instância no ts, ta quase pronto, mas falta:
    
    - Arrumar o estilo com o novo aninhamento de tags do html;
    - Fazer os botões de prev e next funcionarem

--- src/app/components/modal/modal.ts
+++ src/app/components/modal/modal.ts
@@ -1,23 +1,40 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, HostListener, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
   imports: [TranslatePipe],
   templateUrl: './modal.html',
-  styleUrl: './modal.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  styleUrl: './modal.scss'
 })
-export class Modal implements OnInit, OnDestroy {
+export class Modal implements OnInit, OnDestroy, AfterViewInit {
 
   constructor(private http: HttpClient) {}
 
   @Input() id!:number;
   @Output() close = new EventEmitter<void>();
-
   public modalContent:any;
+  swiper!: Swiper;
+
+  ngAfterViewInit(): void {
+    this.swiper = new Swiper('.carousel', {
+      loop: true,
+      speed: 300,
+      navigation: {
+        nextEl: 'swiper-button-next',
+        prevEl: 'swiper-button-prev'
+      },
+      // breakpoints: {
+      //   0: {navigation: false},
+      //   501: {navigation: true},
+      // }
+    })
+
+    setTimeout(() => this.swiper.update(), 200);
+  }
 
   ngOnInit(): void {
     // aplica o estilo para esconder a barra de rolagem (n funciona no .scss)
