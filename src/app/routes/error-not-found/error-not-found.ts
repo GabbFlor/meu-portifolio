11bcbd85cf1a1045626caeb5d2c36d95206f1779
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-11-15 17:22:33 -0300

    feat/fix
    feat: Criando a pag de erro;
    
    Fix: Mudando o favicon

--- src/app/routes/error-not-found/error-not-found.ts
+++ src/app/routes/error-not-found/error-not-found.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { LanguageService } from '../../services/language-service';
+import { ThemeService } from '../../services/theme-service';
 
 @Component({
   selector: 'app-error-not-found',
-  imports: [],
+  imports: [
+    TranslatePipe,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './error-not-found.html',
   styleUrl: './error-not-found.scss'
 })
 export class ErrorNotFound {
+  constructor(
+    public language: LanguageService
+  ) {}
 
+  private themeService = inject(ThemeService)
 }
